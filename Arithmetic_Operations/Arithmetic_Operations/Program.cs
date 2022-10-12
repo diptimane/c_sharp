e9d3725be86f789b0ef06faf508094b9c683540c
Dipti <diptim1732003@gmail.com>
2022-10-12 22:29:25 +0530

    program

---
+++ Arithmetic_Operations/Arithmetic_Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arithmetic_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f;
+            Console.WriteLine("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+            Console.WriteLine("Addition of two number : " +c);
+
+            d = a - b;
+            Console.WriteLine("Substraction of two number : " +d);
+
+            e = a * b;
+            Console.WriteLine("Multiplication of two number : " +e);
+
+            f = a / b;
+            Console.WriteLine("Division of two number : " +f);
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
