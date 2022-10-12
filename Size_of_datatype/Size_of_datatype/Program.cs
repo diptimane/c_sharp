294276a6ff3adbad254300654584eb619f344378
Dipti <diptim1732003@gmail.com>
2022-10-12 22:30:29 +0530

    program

---
+++ Size_of_datatype/Size_of_datatype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Size_of_datatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of char : " + sizeof (char));
+            Console.WriteLine("Size of short : " + sizeof (short));
+            Console.WriteLine("Size of int : " + sizeof (int));
+            Console.WriteLine("Size of double : " + sizeof (double));
+            Console.WriteLine("Size of long : " +sizeof (float));
+
+            Console.ReadLine();
+        }
+    }
+}
